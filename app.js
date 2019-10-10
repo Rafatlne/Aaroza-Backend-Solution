ab46c45b961733754857370291074e18ec5cb986
Rafatlne <ashikrafat.lne@gmail.com>
2019-10-10 15:16:45 +0600

    added error handling in app.js

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const actorRoutes = require('./api/routes/actors');
 const movieRoutes = require('./api/routes/movies');
 const userRoutes = require('./api/routes/users');
 
+// Using morgan package
+app.use(morgan('dev'));
+
+// Routes which should handle requests
 app.use('/actors', actorRoutes);
 app.use('/movies', movieRoutes);
 app.use('/user', userRoutes);
+
+
+// Error Handling 
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404 ;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error : {
+            message: err.message
+        }
+    })
+});
+
 
 module.exports = app;

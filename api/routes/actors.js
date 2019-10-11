442711f2ac6e28f989c110fd7c021bf3346cf5f1
Rafatlne <ashikrafat.lne@gmail.com>
2019-10-11 23:02:03 +0600

    added mongoose

--- api/routes/actors.js
+++ api/routes/actors.js
@@ -8,8 +8,14 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: "this is actor post REQUEST"
+    const actor = {
+        name: req.body.name,
+        birthday: req.body.birthday,
+        country: req.body.country
+    }
+    res.status(201).json({
+        message: "Handling POST requests to  /actors",
+        createdActor: actor
     });
 });
 
--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const actorRoutes = require('./api/routes/actors');
 const movieRoutes = require('./api/routes/movies');
 const userRoutes = require('./api/routes/users');
 
+// Mongoose Connect
+mongoose.connect('mongodb://node-aaroza:'+
+    process.env.MONGO_ATLAS_PW+
+    '@node-aaroza-shard-00-00-mkjpe.mongodb.net:27017,node-aaroza-shard-00-01-mkjpe.mongodb.net:27017,node-aaroza-shard-00-02-mkjpe.mongodb.net:27017/test?ssl=true&replicaSet=node-aaroza-shard-0&authSource=admin&retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true 
+    });
+    
+mongoose.Promise = global.Promise;
+
+
 // Using morgan package
 app.use(morgan('dev'));
+
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// CORS errors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin"
+        ,"Origin, X-Requsted-With, Content-Type, Accept, Authorization");
+
+        if (req.method === 'OPTIONS'){
+            res.header('Access-Control-Allow-Methods', 'GET,PUT, POST, PATCH, DELETE');
+            return res.status(200).json({});
+        }
+    
+        next();
+});
 
 // Routes which should handle requests
 app.use('/actors', actorRoutes);
